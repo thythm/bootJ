8ec602c4f1a82b96c528bbb0bd0fb98f8d3d74f1
thythm <568490210@qq.com>
2018-02-06 14:01:01 +0800

    集成网关：spring cloud gateway，实现路由

---
+++ gateway-service/src/main/java/com/momo/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.momo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * @author majunjie
+ * @date 2018/1/30 15:29
+ */
+@EnableZuulProxy
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
